34c76514c59dd674982023629ff65dea0edeb64d
Tomasz Michalak <tomek.michalak@gmail.com>
2019-07-08 08:53:30 +0200

    Switch to 2.0.0-RC5, removed Gradle scripts included in build.gradle.kts

--- knot.x/build.gradle.kts
+++ knot.x/build.gradle.kts
@@ -47,7 +47,7 @@ tasks.named("build") {
     dependsOn("runTest")
 }
 
-apply(from = "gradle/distribution.gradle.kts")
-apply(from = "gradle/javaAndUnitTests.gradle.kts")
-apply(from = "gradle/docker.gradle.kts")
+apply(from = "https://raw.githubusercontent.com/Knotx/knotx-starter-kit/2.0.0-RC5/gradle/distribution.gradle.kts")
+apply(from = "https://raw.githubusercontent.com/Knotx/knotx-starter-kit/2.0.0-RC5/gradle/javaAndUnitTests.gradle.kts")
+apply(from = "https://raw.githubusercontent.com/Knotx/knotx-starter-kit/2.0.0-RC5/gradle/docker.gradle.kts")
 
